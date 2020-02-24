a627f357cf3a2d6a144c2eed9c6009e530de98f2
sal <salmanamer@gmail.com>
2020-02-24 12:48:33 +0700

    setup project with assets and viewControllers

---
+++ Skate-ARKit/Controllers/RampPickerViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RampPickerViewController.swift
+//  Skate-ARKit
+//
+//  Created by SenorLappy on 24/2/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerViewController: UIViewController {
+
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
