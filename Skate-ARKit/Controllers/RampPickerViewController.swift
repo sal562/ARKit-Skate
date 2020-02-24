d50e229af274cc9f501ab09b43a8488ca65d05c1
sal <salmanamer@gmail.com>
2020-02-24 13:26:30 +0700

    setup 3 objects in AKKit scene as popover

--- Skate-ARKit/Controllers/RampPickerViewController.swift
+++ Skate-ARKit/Controllers/RampPickerViewController.swift
@@ -30,8 +30,38 @@ class RampPickerViewController: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+       
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        // set custom camera /view
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
         preferredContentSize = size
-        // Do any additional setup after loading the view.
+        
+        // add new halfpipe to scene
+        let object = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = object?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.scale = SCNVector3Make(0.0023, 0.0023, 0.0023)
+        node?.position = SCNVector3Make(1, 0.6, -1)
+        scene.rootNode.addChildNode(node!) // adds object as reference - can reuse variables for new object
+    
+    // add new pyramid to scene
+        let object2 = SCNScene(named: "art.scnassets/pyramid.dae")
+        let node2 = object2?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node2?.scale = SCNVector3Make(0.0062, 0.0062, 0.0062)
+        node2?.position = SCNVector3Make(1, -0.6, -0.90)
+        scene.rootNode.addChildNode(node2!) // adds
+        
+        // add new quarterpipe to scene
+          let object3 = SCNScene(named: "art.scnassets/quarter.dae")
+          let node3 = object3?.rootNode.childNode(withName: "quarter", recursively: true)!
+          node3?.scale = SCNVector3Make(0.0062, 0.0062, 0.0062)
+          node3?.position = SCNVector3Make(1, -2.4, -0.70)
+          scene.rootNode.addChildNode(node3!) // adds
+    
     }
     
 
